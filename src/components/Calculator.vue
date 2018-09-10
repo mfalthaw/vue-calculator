d4d9d6560394dbfd8caadaf24683af07a2e20a6e
mfalthaw <mfalthaw@ucalgary.ca>
2018-09-10 14:13:10 +0300

    grid setup

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="hello">
-    <h1>Hello Calc</h1>
+  <div class="calculator">
+    <div class="display">0.02342</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn">=</div>
   </div>
 </template>
 
@@ -11,5 +30,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.calculator {
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
 
+.display {
+  grid-column: 1/5;
+  background-color: red;
+}
+
+.zero {
+  grid-column: 1/3;
+}
 </style>
