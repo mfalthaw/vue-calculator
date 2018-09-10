a2a65f6a04553225391545e8895aa3cd3410513e
mfalthaw <mfalthaw@ucalgary.ca>
2018-09-10 14:15:46 +0300

    init color

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,22 +4,22 @@
     <div class="btn">C</div>
     <div class="btn">+/-</div>
     <div class="btn">%</div>
-    <div class="btn">÷</div>
+    <div class="btn operator">÷</div>
     <div class="btn">7</div>
     <div class="btn">8</div>
     <div class="btn">9</div>
-    <div class="btn">x</div>
+    <div class="btn operator">x</div>
     <div class="btn">4</div>
     <div class="btn">5</div>
     <div class="btn">6</div>
-    <div class="btn">-</div>
+    <div class="btn operator">-</div>
     <div class="btn">1</div>
     <div class="btn">2</div>
     <div class="btn">3</div>
-    <div class="btn">+</div>
+    <div class="btn operator">+</div>
     <div class="btn zero">0</div>
     <div class="btn">.</div>
-    <div class="btn">=</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
@@ -44,5 +44,15 @@ export default {
 
 .zero {
   grid-column: 1/3;
+}
+
+.btn {
+  background-color: #eee;
+  border: 1px solid #333;
+}
+
+.operator {
+  background-color: orange;
+  color: white;
 }
 </style>
